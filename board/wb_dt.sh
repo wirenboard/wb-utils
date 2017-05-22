1136a9fd44f261c48dd8efbbf6539fbf69746c2f
Alexey Ignatov <lexszero@gmail.com>
2017-05-23 01:44:34 +0300

    wb_dt: Add documentation and few more functions

--- board/wb_dt.sh
+++ board/wb_dt.sh
@@ -7,20 +7,30 @@ debug() { :; }
 	>&2 echo "DEBUG: $@"
 }
 
+# Converts binary data to 32bit hex words
 bin2hex() {
 	# This is endianness-sensitive. Works on ARM.
 	od -A n -t x1 -w4 | tr -d ' '
 }
 
+# Converts binary data to 32bit hex words, N words per line
+# Args:
+#	words per line
 bin2hex_width() {
 	od -A n -t x1 -w$((4*$1)) |
 	sed -r 's/ ([0-9a-f]{2}) ([0-9a-f]{2}) ([0-9a-f]{2}) ([0-9a-f]{2})/\1\2\3\4 /g'
 }
 
+# Converts number from hex to decimal
+# Args:
+#	hexadecimal number
 hex2dec() {
 	echo "$((0x${1}))"
 }
 
+# Returns Nth line of the input
+# Args:
+#	line number
 index() {
 	local n="$1"
 	local i=0
@@ -35,18 +45,50 @@ index() {
 	done
 }
 
-dt_get_prop() {
+# Get canonicalized node path in the filesystem
+# Args:
+#	node (from DT root, or full path in /sys/firmware)
+dt_node_path() {
 	local node="$1"
+
+	[[ "${node#/sys/firmware}" == "$node" ]] && node="$DT/$node"
+	echo "$node"
+}
+
+# Get list of node properties matching specified glob pattern
+# Args
+#	node
+#	name glob
+dt_node_props() {
+	local node="$(dt_node_path "$1")"
+	local name="${2:-*}"
+	
+	find "$node" -maxdepth 1 -type f -name "$name" -printf '%f\n'
+}
+
+# Get raw value of property
+# Args:
+#	node
+#	property
+dt_get_prop() {
+	local node="$(dt_node_path "$1")"
 	local prop="$2"
 	
-	[[ "${node#/sys/firmware}" == "$node" ]] && node="$DT/$node"
 	cat "$node/$prop"
 }
 
+# Get hex value of property, one 32bit word per line
+# Args:
+#	node
+#	property
 dt_get_prop_hex() {
 	dt_get_prop "$1" "$2" | bin2hex
 }
 
+# Get unsigned int value of property, one value per line
+# Args:
+#	node
+#	property
 dt_get_prop_int() {
 	local x
 
@@ -55,18 +97,32 @@ dt_get_prop_int() {
 	done
 }
 
+# Get string value of property, one string per line
+# Args:
+#	node
+#	property
 dt_get_prop_str() {
 	dt_get_prop "$1" "$2" | tr '\000' '\n'
 }
 
+# Get compatible list of node, one item per line
+# Args:
+#	node
 dt_get_compatible() {
 	local node="${1:-}"
 	
 	dt_get_prop_str "$node" compatible
 }
 
+# Associative array for GPIO phandle to gpiochip resolving
 declare -A DT_GPIOCHIPS
 
+# Get GPIO(s) property resolving phandles, one GPIO per line.
+# Output line format: "/sys/class/gpio/gpiochip0 10"
+# Args:
+#	node
+#	[optional] property, default is "gpios"
+#	[optional] words per gpio, default is 3
 dt_get_prop_gpio() {
 	local node="$1"
 	local prop="${2:-gpios}"
@@ -79,10 +135,25 @@ dt_get_prop_gpio() {
 	done
 }
 
+# Convert "<gpiochip> <pin>" to Linux GPIO number
+# Args:
+#	gpiochip sysfs path (e.g. "/sys/class/gpio/gpiocip0")
+#	pin number within gpiochip
 gpio_to_num() {
 	local chip="$1"
 	local pin="$2"
 	echo "$(($(cat "$chip/base")+pin))"
+}
+
+# Get GPIO property as Linux gpio number
+# Args:
+#	node
+#	property
+#	[optional] index of GPIO in multi-gpio props
+dt_get_prop_gpionum() {
+	local index="${3:-0}"
+
+	gpio_to_num $(dt_get_prop_gpio "$1" "$2" | index "$index")
 }
 
 dt_parse() {
