8859ade832162e7d0771d33bc09694b185658811
vdromanov <vdromanov@yahoo.com>
2022-11-15 23:56:54 +0300

    introduce journald-logging

--- utils/lib/common.sh
+++ utils/lib/common.sh
@@ -3,14 +3,26 @@
 WB_SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 export WB_DATA_DIR=${WB_DATA_DIR:-/var/lib/wirenboard}
 
-if [[ -n "$DEBUG" ]]; then
-	#set -x
-	set -e
-	debug() {
-		>&2 echo "DEBUG: ${FUNCNAME[1]}: $*"
-	}
-else
+if [[ -z "$DEBUG" ]] && [[ -z "$JOURNALD_PREFIX" ]]; then
 	debug() { :; }
+else
+	#  "of_*:" funcs output is hidden from journald
+	if [[ -n "$DEBUG" ]] && [[ -n "$JOURNALD_PREFIX" ]]; then
+		set -e
+		debug() {
+			>&2 echo "DEBUG: ${FUNCNAME[1]}: $*"
+			echo "${FUNCNAME[1]}: $*" | grep -v "^of_.*:" | systemd-cat -t $JOURNALD_PREFIX
+		}
+	elif [[ -n "$DEBUG" ]]; then
+		set -e
+		debug() {
+			>&2 echo "DEBUG: ${FUNCNAME[1]}: $*"
+		}
+	else
+		debug() {
+			echo "${FUNCNAME[1]}: $*" | grep -v "^of_.*:" | systemd-cat -t $JOURNALD_PREFIX
+		}
+	fi
 fi
 
 die() {
@@ -19,7 +31,7 @@ die() {
 	set +o xtrace
 	local code="${1:-1}"
 	echo "Error in ${BASH_SOURCE[1]}:${BASH_LINENO[0]}. '${BASH_COMMAND}' exited with status $ret"
-	# Print out the stack trace described by $function_stack  
+	# Print out the stack trace described by $function_stack
 	if [[ ${#FUNCNAME[@]} -gt 2 ]]; then
 		echo "Call tree:"
 		for (( i = 1; i < ${#FUNCNAME[@]} - 1; i++)); do
