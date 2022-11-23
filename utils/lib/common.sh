cdf7feab5a91478bdea44a31740b1d89b6c23b53
vdromanov <vdromanov@yahoo.com>
2022-11-23 10:52:11 +0300

    Feature/53876 journald (#61)
    * introduce journald-logging
    
    * logging, from which pid is called
    
    * bump version
    
    * add more comments

--- utils/lib/common.sh
+++ utils/lib/common.sh
@@ -3,14 +3,37 @@
 WB_SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 export WB_DATA_DIR=${WB_DATA_DIR:-/var/lib/wirenboard}
 
-if [[ -n "$DEBUG" ]]; then
-	#set -x
-	set -e
-	debug() {
-		>&2 echo "DEBUG: ${FUNCNAME[1]}: $*"
-	}
-else
+if [[ -z "$DEBUG" ]] && [[ -z "$JOURNALD_PREFIX" ]]; then
 	debug() { :; }
+else
+	# of_*: functions from of.sh and wb_env_of.sh produce tons of hardware-magic output
+	# because of many debug() calls inside
+	# =>
+	# debug_onscreen: echoing to stderr (could suppress later)
+	# debug_journald: systemd-cat accepts only stdin -> grepping "of_"
+	debug_onscreen() {
+		>&2 echo "DEBUG: ${FUNCNAME[2]}: $*"
+	}
+	debug_journald() {
+		echo "${FUNCNAME[2]}: $*" | grep -v "^of_.*:" | systemd-cat -t $JOURNALD_PREFIX
+	}
+
+	if [[ -n "$DEBUG" ]] && [[ -n "$JOURNALD_PREFIX" ]]; then
+		set -e
+		debug() {
+			debug_onscreen $*
+			debug_journald $*
+		}
+	elif [[ -n "$DEBUG" ]]; then
+		set -e
+		debug() {
+			debug_onscreen $*
+		}
+	else
+		debug() {
+			debug_journald $*
+		}
+	fi
 fi
 
 die() {
@@ -19,7 +42,7 @@ die() {
 	set +o xtrace
 	local code="${1:-1}"
 	echo "Error in ${BASH_SOURCE[1]}:${BASH_LINENO[0]}. '${BASH_COMMAND}' exited with status $ret"
-	# Print out the stack trace described by $function_stack  
+	# Print out the stack trace described by $function_stack
 	if [[ ${#FUNCNAME[@]} -gt 2 ]]; then
 		echo "Call tree:"
 		for (( i = 1; i < ${#FUNCNAME[@]} - 1; i++)); do
