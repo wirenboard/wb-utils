7abaafb81d000dda852680dde88bcd3340b307a1
lostpoint-ru <lostpoint@icloud.com>
2022-12-01 16:24:52 +0300

    remove udev rules update start-stop scripts

--- utils/lib/wb-usb-otg/wb-usb-otg-stop.sh
+++ utils/lib/wb-usb-otg/wb-usb-otg-stop.sh
@@ -3,14 +3,18 @@
 DEVDIR=/sys/kernel/config/usb_gadget/g1
 PID_FILE=/var/run/wb-usb-otg.pid
 
-log() {
-    if [ -z "$2" ]; then
+log_target() {
+    if [ -z "$1" ]; then
         level=info
     else
-        level=$2
+        level=$1
     fi
-    echo "$1 ($level)"
-    echo $1 | exec systemd-cat -t wb-usb-otg -p $level
+    exec systemd-cat -t wb-usb-otg -p $level    
+}
+
+log() {
+    echo $1
+    echo $1 | log_target $2
 }
 
 log "wb-usb-otg-stop"
@@ -28,34 +32,34 @@ else
     exit
 fi
 
-echo '' > $DEVDIR/UDC
+echo '' > $DEVDIR/UDC | log_target
 
-[ -L $DEVDIR/os_desc/c.1 ] && rm $DEVDIR/os_desc/c.1
+[ -L $DEVDIR/os_desc/c.1 ] && rm $DEVDIR/os_desc/c.1 | log_target
 
 log "Removing strings from configurations"
 for dir in $DEVDIR/configs/*/strings/*; do
-    [ -d $dir ] && rmdir $dir >> $LOG_FILE 2>&1
+    [ -d $dir ] && rmdir $dir | log_target
 done
 
 log "Removing functions from configurations"
 for func in $DEVDIR/configs/*.*/*.*; do
-    [ -e $func ] && rm $func >> $LOG_FILE 2>&1
+    [ -e $func ] && rm $func | log_target
 done
 
 log "Removing configurations"
 for conf in $DEVDIR/configs/*; do
-    [ -d $conf ] && rmdir $conf >> $LOG_FILE 2>&1
+    [ -d $conf ] && rmdir $conf | log_target
 done
 
 log "Removing functions"
 for func in $DEVDIR/functions/*.*; do
-    [ -d $func ] && rmdir $func >> $LOG_FILE 2>&1
+    [ -d $func ] && rmdir $func | log_target
 done
 
 log "Removing strings"
 for str in $DEVDIR/strings/*; do
-    [ -d $str ] && rmdir $str >> $LOG_FILE 2>&1
+    [ -d $str ] && rmdir $str | log_target
 done
 
 log "Removing gadget"
-rmdir $DEVDIR >> $LOG_FILE 2>&1
+rmdir $DEVDIR | log_target
