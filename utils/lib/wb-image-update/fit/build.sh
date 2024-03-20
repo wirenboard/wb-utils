f456e500341ae7c1a2cc79f590e74dc5d609ce33
Nikita Maslov <nikita.maslov@wirenboard.com>
2024-03-20 19:28:45 +0500

    fix of_machine_match for wb8 bootlet

--- utils/lib/wb-image-update/fit/build.sh
+++ utils/lib/wb-image-update/fit/build.sh
@@ -90,7 +90,7 @@ echo -n "+force-repartition " >> /var/lib/wb-image-update/firmware-compatible
 echo -n "+repartition-ramsize-fix " >> /var/lib/wb-image-update/firmware-compatible
 
 if [[ ! -f /var/lib/wb-image-update/zImage ]] || [[ ! -f /var/lib/wb-image-update/boot.dtb ]]; then
-    if ! of_machine_match "wirenboard,wirenboard8"; then
+    if ! of_machine_match "wirenboard,wirenboard-8xx"; then
         echo "bootlet is not found, something went wrong"
         exit 1
     fi
