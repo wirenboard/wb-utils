986b266ac67ac18e73a6b8a5cef4cc312f970f81
Nikita Maslov <nikita.maslov@wirenboard.com>
2024-05-21 16:00:50 +0500

    add comment about missing compatibility entry in check-if-supports-usb-network.sh

--- utils/lib/wb-usb-otg/check-if-supports-usb-network.sh
+++ utils/lib/wb-usb-otg/check-if-supports-usb-network.sh
@@ -4,6 +4,8 @@
 
 wb_source "of"
 
+# wirenboard-720 matches all Wiren Board 7.x boards, that all is because of missing entry in earlier DTBs
+
 if of_machine_match "wirenboard,wirenboard-720" || of_machine_match "wirenboard,wirenboard-84x"; then
     exit 0
 fi
