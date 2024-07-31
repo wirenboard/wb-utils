ca98cef6ef0d3235d455ba327b14f88a5a7d7d4e
vdromanov <vdromanov@yahoo.com>
2024-07-31 18:47:43 +0300

    fix wb-usb-otg on wb85

--- utils/lib/wb-usb-otg/check-if-supports-usb-network.sh
+++ utils/lib/wb-usb-otg/check-if-supports-usb-network.sh
@@ -6,7 +6,7 @@ wb_source "of"
 
 # wirenboard-720 matches all Wiren Board 7.x boards, that all is because of missing entry in earlier DTBs
 
-if of_machine_match "wirenboard,wirenboard-720" || of_machine_match "wirenboard,wirenboard-84x"; then
+if of_machine_match "wirenboard,wirenboard-720" || of_machine_match "wirenboard,wirenboard-8xx"; then
     exit 0
 fi
 
